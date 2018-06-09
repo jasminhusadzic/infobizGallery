004feb4f36e29de5d738840ecee6f7a14f8acfea
Jasmin Husadzic <jasmin@comtech.ba>
2018-06-09 15:49:10 +0200

    set up gallery vol 2

--- gallery/src/app/app.module.ts
+++ gallery/src/app/app.module.ts
@@ -3,18 +3,25 @@ import { NgModule } from '@angular/core';
 import { routes } from './app.router';
 import { NgxGalleryModule } from 'ngx-gallery';
 
+import 'hammerjs'; // <------ mandatory dependency for angular-modal-gallery
+import 'mousetrap'; // <------ mandatory dependency for angular-modal-gallery
+import { ModalGalleryModule } from 'angular-modal-gallery';
+
 import { AppComponent } from './app.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { Gallery2Component } from './gallery2/gallery2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GalleryComponent
+    GalleryComponent,
+    Gallery2Component
   ],
   imports: [
     BrowserModule,
     routes,
-    NgxGalleryModule
+    NgxGalleryModule,
+    ModalGalleryModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- gallery/src/app/app.router.ts
+++ gallery/src/app/app.router.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule} from '@angular/router';
 
 import { AppComponent} from './app.component';
 import { GalleryComponent} from './gallery/gallery.component';
+import { Gallery2Component} from './gallery2/gallery2.component';
 
 export const router: Routes = [
-    {path:'gallery', component: GalleryComponent}
+    {path:'gallery', component: GalleryComponent},
+    {path:'gallery2', component: Gallery2Component}  
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
